e1ab893fd25ad458d3cf44eef8ffc5bdd57a81ed
javerdejo <javier_arellano_verdejo@hotmail.com>
2017-02-03 21:05:38 +0100

    Signal (ctrl+c) support added

--- read_log.py
+++ read_log.py
@@ -1,35 +1,45 @@
+import signal
 import time
 import json
+import sys
 import os
 
-path = ''
+def signal_handler(signal, frame):
+    print " "
+    print "Closing log file"
+    fin.close()
+    print "Bye ;)"
+    sys.exit(0)
+
+path = './'
 name_list = os.listdir(path)
 full_list = [os.path.join(path,i) for i in name_list]
 time_sorted_list = sorted(full_list, key=os.path.getmtime)
 fname = [os.path.basename(i) for i in time_sorted_list]
-print fname[-1]
 
 fin = open(fname[-1], "r")
-fUpdated = False
+logUpdated = False
 
-print "Reading log name: " +  fin.name
+signal.signal(signal.SIGINT, signal_handler)
+print "----------------------------------------------------------------------"
+print "Elite Dangerous Panel Event Monitor"
+print "Logfile name: " +  fin.name
+print "Press Ctrl+C to exit"
+print "----------------------------------------------------------------------"
+
 while True:
     line = fin.readline()
     if not line:
         time.sleep(1)
-        if fUpdated:
-            print "Current star: " + starSystem
-            print "Current station: " + stationName
+        if logUpdated:
+            print "> Star (" + starSystem + ") --- Station (" + stationName +")"
             print " "
-            fUpdated = False
+            logUpdated = False
     else:
         data = json.loads(line)
         if data["event"] == "Docked":
             stationName = data["StationName"]
-            fUpdated = True
+            logUpdated = True
         elif data["event"] == "SupercruiseExit":
             starSystem = data["StarSystem"]
-            fUpdated = True
-
-
-fin.close()
+            logUpdated = True
